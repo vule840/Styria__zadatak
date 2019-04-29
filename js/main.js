13407711382201258616fe4a2a199e89f10316f0
vule840 <vule840@gmail.com>
2019-04-30 00:23:20 +0200

    2

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ console.log('sdfsdf')
 
 
 
-var vm  = new Vue({
+new Vue({
 
 	el:'#vue-app',
 	
@@ -15,7 +15,7 @@ var vm  = new Vue({
 	
 	},
 	created(){
-		 this.$http.get('players.json').then(function(data){
+		 this.$http.get('json/players.json').then(function(data){
 		 	console.log(data.body.players)
 		 	this.lasts = data.body.players.reverse().slice(-1)[0];
 		 	this.players = data.body.players.slice(0,7);
@@ -27,27 +27,69 @@ var vm  = new Vue({
 		// 	this.players = response.body;
 		// 	console.log(players)
 		// })
+	},
+	methods:{
+	 	sortaj: function (event) {
+	 			// this.players = data.body.players.sort()
+	 			console.log(event)
+	 			console.log(event.target.innerText)
+	 			this.count = event.target.innerText;
+	 	},
+	 	testiraj: function(event){
+	 		event.preventDefault()
+	 		console.log(event.srcElement.innerText)
+	 		this.korisnik = event.target.innerText;
+	 		 if (event.target.classList === "col tablica__score") {
+
+	 			console.log('tablica__score')
+	 		 } else if(event.target.classList === "col tablica__name"){
+	 		 	console.log('tablica__name')	
+
+	 		 }
+	 	}
 	 }
+
+	// mounted(){
+	// 	// Create a request variable and assign a new XMLHttpRequest object to it.
+	// 		var upit = new XMLHttpRequest();
+	// 		// Open a new connection, using the GET request on the URL endpoint
+	// 		upit.open('GET', 'players.json', true);
+	// 		upit.onload = function () {
+	// 		  // Begin accessing JSON data here
+	// 		  // Begin accessing JSON data here
+	// 		var data = JSON.parse(this.response);
+	// 		console.log(data)
+	// 		console.log(data.players)
+	// 		// data.forEach(player => {
+	// 		//   // Log each movie's title
+	// 		//   console.log(player);
+	// 		// });
+	// 		  }
+	// 		// Send request
+	// 		upit.send();
+	// }
 	// methods:{
-	//  	sortaj: function (event) {
-	//  			// this.players = data.body.players.sort()
-	//  			console.log(event)
-	//  			console.log(event.target.innerText)
-	//  			this.count = event.target.innerText;
-	//  	},
-	//  	testiraj: function(event){
-	//  		event.preventDefault()
-	//  		console.log(event.srcElement.innerText)
-	//  		this.korisnik = event.target.innerText;
-	//  		 if (event.target.classList === "col tablica__score") {
-
-	//  			console.log('tablica__score')
-	//  		 } else if(event.target.classList === "col tablica__name"){
-	//  		 	console.log('tablica__name')	
-
-	//  		 }
-	//  	}
-	//  }
-
+	// 	greet:function(time){
+	// 		return 'Dobro' + time + ' ' + this.name;
+	// 	},
+	// 	//add and substract
+	// 	add:function(inc){
+	// 		this.age += inc;
+	// 	},
+	// 	substract:function(dec){
+	// 		this.age -= dec;
+	// 	},
+	// 	updateXY:function(event){
+	// 		this.x = event.offsetX;
+	// 		this.y = event.offsetY;	
+	// 	},
+	// 	click:function(){
+	// 		alert("joooo");
+	// 	},
+	// 	dodaj:function(){
+	// 		alert('dodaj nestš')
+	// 	}
+	// }
 	
 })
+
